aa6c8f77797dc06c73b271f6cc6a84996c3c25b6
suryapratap1051 <suryapratap1051@gmail.com>
2024-11-08 19:45:42 +0530

    gg

--- newsblogs/settings.py
+++ newsblogs/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-&n1d*-#iv+bolpx7+g8rkhybdbq@z1an+g06a+&!e1=q2c6=%d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
